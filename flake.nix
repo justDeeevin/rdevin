7795e309d6852e05c5a6f25c49c32b0eac4bbe24
Devin Droddy <devin.droddy@gmail.com>
2025-04-16 14:17:01 -0400

    flake for devshell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+      runtimeLibs = with pkgs; [
+        xorg.libX11
+        xorg.libXi
+        xorg.libXtst
+      ];
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+        ] ++ runtimeLibs;
+        nativeBuildInputs = [ pkgs.pkg-config ];
+
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeLibs;
+      };
+    };
+}
